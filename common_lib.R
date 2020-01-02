869986009781c2a259ab07f349dc0edb6f615a14
Kostas Lagogiannis <costaslag@gmail.com>
2020-01-02 13:34:23 +0000

    Moved funct to common lib - Capt Speed Clustering

--- common_lib.R
+++ common_lib.R
@@ -57,6 +57,7 @@ colourLegE <- c("#FB9A99C8","#B2DF8AC8","#A6CEE3C8","#FDBF6F23")
 colourLegL <- c("#E31A1CD3", "#33A02CD3", "#1F78B4D3", "#FF7F00D3") 
 colourL    <-c("#03B303AF","#E60303AF","#0303E6AF")
 colourHPoint <- c("#E31A1C37","#33A02C37","#1F78B437","#FB9A9937","#B2DF8A37","#A6CEE337","#FDBF6F37")
+colourHLine <- colourLegL
 
 pchL <- c(1,2,0,17,15,16,4) ## The style of bullet used for each group DL, LL, NL
 lineTypeL <- c(2,1,3,4) ## The style of bullet used for each group DL, LL, NL
@@ -65,3 +66,141 @@ lineTypeL <- c(2,1,3,4) ## The style of bullet used for each group DL, LL, NL
 strDataLabels <- expression("NF-s","LF-s","DF-s","NF-e","LF-e","DF-e" )
 strGroupID <- c("DL","LL","NL") 
 ## GLOBAL VARS ###
+
+
+########### Capture Type Clustering Functions ####
+##################################################
+
+## Plots the Data Density and the 2 Gaussians fititng high and low speed capture swims
+plotCaptureSpeedFit <- function(datSpeed,drawMCMC,colourIdx,nchain = 1)
+{
+  xquant <- seq(0,70,1)
+  XLIM <- c(0,60)
+  YLIM <- c(0,0.15)
+  pdistBW <- 5 ## mm/sec
+  strKern <- "gaussian"
+  #ntail <- NROW(drawMCMC$mu[1,2,,nchain])*0.10
+  ntail <- min(50,NROW(drawMCMC$mu[1,1,,1])*0.10)
+  
+  plot(density(datSpeed$CaptureSpeed,bw=pdistBW,kernel=strKern),col="black",lwd=4,xlim=XLIM,ylim=YLIM,cex=cex,cex.axis=cex 
+       ,main=NA,xlab = NA,ylab=NA)
+  for (i in 1:(ntail-1) )
+  {
+    lines(xquant,dnorm(xquant,mean=tail(drawMCMC$mu[1,2,ntail-i,nchain],1),sd=tail(drawMCMC$sigma[1,2,ntail-i,nchain],1)),type='l',col=colourHLine[colourIdx],lty=1 )
+    lines(xquant,dnorm(xquant,mean=tail(drawMCMC$mu[2,2,ntail-i,nchain],1),sd=tail(drawMCMC$sigma[2,2,ntail-i,nchain],1)),type='l',col=colourHLine[colourIdx],lty=2 )
+  }
+  
+  dens<- density(datSpeed$CaptureSpeed,bw=pdistBW,kernel=strKern)
+  lines(dens,col="black",lwd=4,xlim=XLIM )
+  legend("topright",title="",cex=cex,
+         legend=c( paste0("",dens$n, "# Data density "), #(Bw:",prettyNum(digits=2, pdistBW ),")" ) ,
+                   paste("Model low speed " ),
+                   paste("Model high speed " )),
+         col=c("black",colourLegL[colourIdx],colourLegL[colourIdx]),lwd=c(3,1,1),lty=c(1,1,2) ) 
+  
+  mtext(side = 1,cex=cex, line = 3.2, expression("Capture speed (mm/sec) " ))
+  mtext(side = 2,cex=cex, line = 2.5, expression("Density function " ))
+  
+}
+
+
+
+###Used for drawing contour in ggplot -
+## Draw the model fit above the cluster points
+getFastClusterGrid <- function(drawMCMC)
+{
+  ### Add the cluster contours ###
+  xran <- seq(0,0.8,0.05) ##Distance Grid
+  yran <- seq(0,70,1) ##Speed Grid
+  
+  ##Example Code for PLotting Inferred Multivariate Cluster
+  nsteps <- NROW(drawMCMC$mID[,,1][1,])
+  mat_cov_fast <- rowMeans(drawMCMC$cov[2,,,(nsteps-1000):nsteps,1],dim=2) ##Average over samples
+  
+  mat_mu <- rowMeans(drawMCMC$mu[,,(nsteps-1000):nsteps,1],dim=2)
+  #valGrid <- matrix( expand.grid(distance=xran,speed=yran),nrow=NROW(xran),ncol=NROW(yran) )
+  valGrid <- expand.grid(distance=xran,speed=yran)
+  #matrix(valGrid$distance,ncol=NROW(xran))
+  cluster_z <- mvtnorm::dmvnorm(valGrid,mean=mat_mu[2,],sigma=mat_cov_fast )
+  cluster_fast <- cbind(valGrid,cluster_z, factor(rep(16,NROW(valGrid) ),levels=c(1,16),labels=c("slow","fast")  ) )
+  
+  names(cluster_fast) <- c("DistanceToPrey", "CaptureSpeed", "Density","Cluster")
+  
+  return(cluster_fast)
+}
+
+###Used for drawing contour in ggplot
+getSlowClusterGrid <- function(drawMCMC)
+{
+  ### Add the cluster contours ###
+  xran <- seq(0,0.8,0.05) ##Distance Grid
+  yran <- seq(0,70,1) ##Speed Grid
+  
+  ##Example Code for PLotting Inferred Multivariate Cluster
+  nsteps <- NROW(drawMCMC$mID[,,1][1,])
+  mat_cov_slow <- rowMeans(drawMCMC$cov[1,,,(nsteps-1000):nsteps,1],dim=2) ##Average over samples
+  
+  mat_mu <- rowMeans(drawMCMC$mu[,,(nsteps-1000):nsteps,1],dim=2)
+  #valGrid <- matrix( expand.grid(distance=xran,speed=yran),nrow=NROW(xran),ncol=NROW(yran) )
+  valGrid <- expand.grid(distance=xran,speed=yran)
+  #matrix(valGrid$distance,ncol=NROW(xran))
+  cluster_z <- mvtnorm::dmvnorm(valGrid,mean=mat_mu[1,],sigma=mat_cov_slow )
+  cluster_slow <- cbind(valGrid,cluster_z, factor(rep(1,NROW(valGrid) ),levels=c(1,16),labels=c("slow","fast")  ) )
+  
+  names(cluster_slow) <- c("DistanceToPrey", "CaptureSpeed", "Density","Cluster")
+  
+  return(cluster_slow)
+}
+
+
+
+
+## Uses the sampled occupancy for cluster membership, assigns cluster depending on whether the 
+# average time spend in fast cluster (mID=1) is above minClusterLikelyhood
+addClusterColumns <- function(datDistVsSpeed,draw_F)
+{
+  ## Set Cluster
+  minClusterLikelyhood <- 0.7
+  nsamples <- 200
+  ch <- 1
+  tsamples <- NROW(draw_F$mID[1,,ch]) ##Get Total Number of Samples in Chain
+  
+  
+  ###Add Cluster Column / Set According to mean occupancy being above 0.7
+  datDistVsSpeed$fastClustScore <- apply(draw_F$mID[,(tsamples-nsamples):tsamples,ch],1,mean) 
+  datDistVsSpeed$pchL <- 1
+  datDistVsSpeed$pchL[datDistVsSpeed$fastClustScore > minClusterLikelyhood] <- 16 ##Set marker Point Type used for display
+  datDistVsSpeed$Cluster <- factor(labels=c("slow","fast"),datDistVsSpeed$pchL)  ##Define Cluster type factor
+  return (datDistVsSpeed)
+}
+### Capture Type Clusting plot as in Figure 4 function to show the inferred Gaussian models for capture type clustering
+## the data points coloured according to cluster membership along with their densities on the plot margins 
+## \note Uses ggplot
+plotClusteredData <- function(distVsSpeed_F,draw_F)
+{
+  distVsSpeed_F <- addClusterColumns(distVsSpeed_F,draw_F)
+  
+  p_NF = ggplot( distVsSpeed_F, aes(DistanceToPrey, CaptureSpeed,color =Cluster,fill=Cluster))  +
+    ggtitle(NULL) +
+    theme(axis.title =  element_text(family="Helvetica",face="bold", size=16),
+          axis.text = element_text(family="Helvetica",face="bold", size=16),
+          plot.margin = unit(c(1,1,1,1), "mm"), legend.position = "none") +
+    fill_palette("jco")
+  
+  p_NF = p_NF + geom_point( size = 3, alpha = 0.6,aes(color =distVsSpeed_F$Cluster) ) +  xlim(0, 0.8) +  ylim(0, 80) +
+    scale_color_manual( values = c("#00AFBB", "#E7B800", "#FC4E07") )
+  
+  contour_fast <- getFastClusterGrid(draw_F) ## Draw the mvtnorm model fit contour
+  contour_slow <- getSlowClusterGrid(draw_F)
+  p_NF = p_NF +
+    geom_contour(contour_fast, mapping = aes(x = DistanceToPrey, y = CaptureSpeed, z = Density) ,linetype=2 ) +
+    geom_contour(contour_slow, mapping = aes(x = DistanceToPrey, y = CaptureSpeed, z = Density) ,linetype=2 ) +
+    scale_x_continuous(name="Distance to prey (mm)", limits=c(0, 0.8)) +
+    scale_y_continuous(name="Capture Speed (mm/sec)", limits=c(0, 80)) 
+  #theme_linedraw()
+  
+  ggMarginal(p_NF, x="DistanceToPrey",y="CaptureSpeed", type = "density",groupColour = TRUE,groupFill=TRUE,show.legend=FALSE) 
+}  
+
+################################
+
